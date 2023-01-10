55289fdc7dc62314838d23f3f543d34459a52c16
AswathiMohan23 <achu999here@gmail.com>
2023-01-10 12:56:19 +0530

    [ASWATHI] added : code to find the largest number using if else

---
+++ ifElse/vowel.py
@@ -0,0 +1,5 @@
+alphabet = input("enter the alphabet in small letter : ")
+if alphabet == 'a' or alphabet == 'e' or alphabet == 'i' or alphabet == 'o' or alphabet == 'u':
+    print("Its a vowel")
+else:
+    print("Its not a vowel")
