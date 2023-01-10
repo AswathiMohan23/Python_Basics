f9c106fada785a382c323e147b2a5f9000e77de3
AswathiMohan23 <achu999here@gmail.com>
2023-01-10 14:04:18 +0530

    [ASWATHI] added : code to display the result of each arithmatic operations

---
+++ Operators/ArithmaticOperators.py
@@ -0,0 +1,12 @@
+firstNo = int(input("enter the first no : "))
+secondNo = int(input("enter the first no : "))
+print("The sum = ", firstNo+secondNo)
+print("The difference = ", secondNo-firstNo)
+print("The product = ", firstNo*secondNo)
+print("The quotient = ", secondNo/firstNo)
+print("The reminder  = ", secondNo % firstNo)
+print("The result of ", secondNo, " ** ", firstNo, "  = ", secondNo ** firstNo)
+print("floor division result = ", firstNo // secondNo)
+
+
+
