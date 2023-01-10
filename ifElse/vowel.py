53c46b97ae9b1047484c5c58934bdff4d84d114f
AswathiMohan23 <achu999here@gmail.com>
2023-01-10 12:57:17 +0530

    [ASWATHI] added : code to check whether an alphabet is a vowel using if-lse

--- ifElse/vowel.py
+++ ifElse/vowel.py
@@ -1,5 +1,6 @@
 alphabet = input("enter the alphabet in small letter : ")
 if alphabet == 'a' or alphabet == 'e' or alphabet == 'i' or alphabet == 'o' or alphabet == 'u':
     print("Its a vowel")
+
 else:
     print("Its not a vowel")
