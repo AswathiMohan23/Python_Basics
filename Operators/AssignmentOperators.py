f2cc2a6959d8e09f96a083c6c57b6b05d12d2557
AswathiMohan23 <achu999here@gmail.com>
2023-01-10 14:18:08 +0530

    [ASWATHI] added : example for assignment operators

---
+++ Operators/AssignmentOperators.py
@@ -0,0 +1,18 @@
+number = int(input("enter the  no : "))
+print("the number = ", number)
+number = number+1
+print("number=number+1 ------> ", number)
+
+number = number-1
+print("number=number-1 ------> ", number)
+
+number = number*1
+print("number=number*1 ------> ", number)
+
+number = number/1
+print("number=number/1 ------> ", number)
+
+a=8
+b=3
+number = a % b
+print("number=8%3------> ", number)
