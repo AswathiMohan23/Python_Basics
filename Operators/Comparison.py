5f5331edc042dbb64e2bf4eb3f01a3990122af44
AswathiMohan23 <achu999here@gmail.com>
2023-01-10 14:23:36 +0530

    [ASWATHI] added : example for comparison operator

---
+++ Operators/Comparison.py
@@ -0,0 +1,8 @@
+firstNo = int(input("enter a number between 1 - 10 : "))
+if firstNo == 5:
+    print("the number is 5")
+elif firstNo < 5:
+    print("the number is  less than 5")
+elif firstNo > 5:
+    print("the number is  greater than 5")
+
