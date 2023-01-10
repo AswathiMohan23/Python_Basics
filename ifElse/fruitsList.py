24059b50894383b0c7d2a30bc76287c6b7fd9220
AswathiMohan23 <achu999here@gmail.com>
2023-01-10 12:51:39 +0530

    [ASWATHI] added: code to find whether a fruit is present in the given list using if else

---
+++ ifElse/fruitsList.py
@@ -0,0 +1,6 @@
+list=['apple','orange','Mango','kiwi','grapes']
+fruit=input("Enter the name of a fruit : ")
+if fruit in list:
+    print(fruit," is present in the given list ",list)
+else:
+    print(fruit," is not present in the given list ",list)
