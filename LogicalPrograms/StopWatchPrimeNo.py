60beafde54e4a8941f4c6dd539bfc6221095065c
AswathiMohan23 <achu999here@gmail.com>
2023-01-11 10:04:11 +0530

    [ASWATHI] added : code to find the ellapsed time in the prime number program

---
+++ LogicalPrograms/StopWatchPrimeNo.py
@@ -0,0 +1,19 @@
+import time
+
+start = time.time()  # can also use timer()
+flag = 0
+number = int(input("Enter the number : "))
+
+if number == 0 or number == 1:
+    print("Number is neither prime nor composite")
+else:
+    for i in range(2, number):
+        if number % i == 0:
+            flag = 1
+            break
+    if flag == 0:
+        print("Number is a prime number")
+    elif flag == 1:
+        print("Number is a composite number")
+end = time.time()
+print(end - start)
