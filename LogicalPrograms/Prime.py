7b0856a533138cb6bd0dcb5aa009d15ac999377b
AswathiMohan23 <achu999here@gmail.com>
2023-01-11 09:53:47 +0530

    [ASWATHI] REFACTORED : the code to check whether a no is prime or composite

--- LogicalPrograms/Prime.py
+++ LogicalPrograms/Prime.py
@@ -1,19 +1,13 @@
 flag = 0
 number = int(input("Enter the number : "))
-for i in range(2, number):
-    if number % i == 0:
-        flag = 1
-        break
-if number == 1 or number == 0:
+if number == 0 or number == 1:
     print("Number is neither prime nor composite")
-elif flag != 1:
-    print("Number is a prime number")
 else:
-    print("Number is a composite number")
-
-
-
-
-
-
-
+    for i in range(2, number):
+        if number % i == 0:
+            flag = 1
+            break
+    if flag == 0:
+        print("Number is a prime number")
+    elif flag == 1:
+        print("Number is a composite number")
