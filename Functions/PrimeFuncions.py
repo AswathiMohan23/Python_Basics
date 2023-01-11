247b4a5a66b47da4e086c8ca05806bb90826b42d
AswathiMohan23 <achu999here@gmail.com>
2023-01-11 15:38:02 +0530

    [ASWATHI] added : code to check whether a no is prime or not using functions

---
+++ Functions/PrimeFuncions.py
@@ -0,0 +1,17 @@
+def prime(number):
+    flag = 0
+    if number == 0 or number == 1:
+        print("Number is neither prime nor composite")
+    else:
+        for i in range(2, number):
+            if number % i == 0:
+                flag = 1
+                break
+        if flag == 0:
+            print("Number is a prime number")
+        elif flag == 1:
+            print("Number is a composite number")
+
+
+value = int(input("Enter the number : "))
+prime(value)
