65f1947b5620f5598a2044502ff16b859fa055ce
AswathiMohan23 <achu999here@gmail.com>
2023-01-16 08:59:55 +0530

    [ASWATHI] refactored . code using set comprehension

--- Comprehensions/set_comprehensions.py
+++ Comprehensions/set_comprehensions.py
@@ -8,3 +8,12 @@ for i in input_list:
         output_set.add(i)
 
 print("Even no set : ",output_set)
+
+print("===================== using comprehension =============================")
+
+input_list = [1, 2, 3, 4, 4, 5, 6, 6, 6, 7, 7]
+
+set_using_comp = {var for var in input_list if var % 2 == 0}
+
+print("Output Set using set comprehensions:",
+      set_using_comp)
