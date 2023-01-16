75007c07a1f34ca229db675d8e41276d8bf71490
AswathiMohan23 <achu999here@gmail.com>
2023-01-16 12:25:48 +0530

    [ASWATHI] added . code to pop elements from a list

---
+++ DataStructures/list_pop.py
@@ -0,0 +1,5 @@
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+number.pop(0)
+print("After removing element at index 0 using pop() ===> ", number)
+number.pop()   # if index is not specified the it reoves the last element in the list
+print("The list without specifying index in pop ===> ",number)
