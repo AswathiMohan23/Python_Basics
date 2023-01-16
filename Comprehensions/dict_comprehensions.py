80a1bb18959e3a544df7bb7bd1c9baba253e6985
AswathiMohan23 <achu999here@gmail.com>
2023-01-16 09:01:46 +0530

    [ASWATHI] refactored . code using dictionary comprehension

--- Comprehensions/dict_comprehensions.py
+++ Comprehensions/dict_comprehensions.py
@@ -9,3 +9,13 @@ for i in input_list:
 
 print("Output Dictionary using for loop:",
       output_dict)
+
+# Using Dictionary comprehensions
+# for constructing output dictionary
+
+input_list = [1, 2, 3, 4, 5, 6, 7]
+
+dict_using_comp = {var for var in input_list if var % 2 == 0}
+
+print("Output Dictionary using dictionary comprehensions:",
+      dict_using_comp)
