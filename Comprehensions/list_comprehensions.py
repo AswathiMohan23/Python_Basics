55f710596aae5c3fdd48cf57ca2491c77193a964
AswathiMohan23 <achu999here@gmail.com>
2023-01-16 08:58:05 +0530

    [ASWATHI] refactored . code using list comprehension

--- Comprehensions/list_comprehensions.py
+++ Comprehensions/list_comprehensions.py
@@ -8,3 +8,10 @@ for i in input_list:
         output_list.append(i)
 
 print("Output List using for loop:", output_list)
+print("===================== using comprehension =============================")
+input_list = [1, 2, 3, 4, 4, 5, 6, 7, 7]
+
+list_using_comp = [var for var in input_list if var % 2 == 0]
+
+print("Output List using list comprehensions:",
+      list_using_comp)
