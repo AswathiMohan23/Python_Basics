0fc5861accacf75e5487bac05c0f44ae4608fec3
AswathiMohan23 <achu999here@gmail.com>
2023-01-16 12:26:57 +0530

    [ASWATHI] added . code to sort a list

---
+++ DataStructures/sortList.py
@@ -0,0 +1,8 @@
+num_list = [34, 2, 67, 4, 13, 29, 10, 20, 66]
+num_list.sort()
+print(num_list) # sorted in ascending order
+
+num_list2 = [34, 2, 67, 4, 13, 29, 10, 20, 66]
+num_list2.sort(reverse=True)
+print(num_list2)
+
