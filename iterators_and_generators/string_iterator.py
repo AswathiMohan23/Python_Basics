d7b5e1ac97fc1eaa7e5e486190978cd836e0d340
AswathiMohan23 <achu999here@gmail.com>
2023-01-17 12:29:51 +0530

    [ASWATHI] added . code to iterate a string using iterator

---
+++ iterators_and_generators/string_iterator.py
@@ -0,0 +1,8 @@
+bird = 'parrot'
+bird_iterator = iter(bird)
+print(next(bird_iterator))
+print(next(bird_iterator))
+print(next(bird_iterator))
+print(next(bird_iterator))
+print(next(bird_iterator))
+print(next(bird_iterator))
