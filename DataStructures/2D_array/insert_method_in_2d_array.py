0659261104c5900e901828b845913610611f2a3d
AswathiMohan23 <achu999here@gmail.com>
2023-03-26 09:03:02 +0530

    [ASWATHI] ADDED . code to to insert a new array in the existing 2d array using insert()

---
+++ DataStructures/2D_array/insert_method_in_2d_array.py
@@ -0,0 +1,13 @@
+def display_output(num_array):
+    for i in num_array:
+        for j in i:
+            print(j,end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    num_array = [[1, 3, 5, 6], [4, 9, 8, 7], [2, 8, 7], [9, 8, 2, 1, 3]]
+    display_output(num_array)
+    print("\n\t\tAfter inserting a new array the output is \n")
+    num_array.insert(2,[10,20,30,40])
+    display_output(num_array)
