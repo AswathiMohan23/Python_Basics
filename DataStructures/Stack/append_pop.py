deb3f2f39baf3969a8c0753ba83e0ff5bebc1cf4
AswathiMohan23 <achu999here@gmail.com>
2023-03-26 10:23:46 +0530

    [ASWATHI] ADDED . code to append elements to stack and pop elements from stack

---
+++ DataStructures/Stack/append_pop.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    stack_data = []
+    stack_data.append('a')
+    stack_data.append('b')
+    stack_data.append('c')
+    stack_data.append('d')
+    print("after appending data to stack_data : ",stack_data)
+    stack_data.pop()
+    stack_data.pop()
+    print("after giving pop() for two times the output is : ",stack_data)
